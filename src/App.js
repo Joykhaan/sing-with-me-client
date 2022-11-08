2afe63f4958a39c73766774479d3180cacea4ff0
Joykhaan <perfectkhan421@gmail.com>
2022-11-09 00:40:43 +0600

    banner done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Pages/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn">Button</button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
